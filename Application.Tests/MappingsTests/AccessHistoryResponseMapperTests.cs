0d7964d8d3564b418dcc545385cfc2f773233eb7
Rameez Ahmed <rameezahmed349@hotmail.com>
2022-12-22 12:27:33 +0500

    Added unit tests for application services.

---
+++ Application.Tests/MappingsTests/AccessHistoryResponseMapperTests.cs
@@ -0,0 +1,39 @@
+﻿using Application.Dtos.Responses;
+using Application.Mappings;
+using Domain.Entities;
+using FluentAssertions;
+
+namespace Application.Tests.MappingsTests
+{
+    public class AccessHistoryResponseMapperTests
+    {
+        [Fact]
+        public void Test_MapToAccessHistoryResponseDto()
+        {
+            DateTime timestamp = DateTime.UtcNow;
+            var accessHistory = new AccessHistory
+            {
+                UserId = 1,
+                DoorId = 1,
+                AccessGranted = true,
+                Timestamp = timestamp,
+                Door = new Door { Id = 1, Name = "Entrance" },
+                User = new User { Id = 1, Name = "Rameez" }
+            };
+
+            var accessHistoryMappedDto = accessHistory.MapToAccessHistoryResponseDto();
+
+            var expectedResult = new AccessHistoryResponse
+            {
+                AccessGranted = true,
+                DoorId = 1,
+                UserId = 1,
+                Doorname = "Entrance",
+                Username = "Rameez",
+                Timestamp = timestamp
+            };
+
+            accessHistoryMappedDto.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ Application.Tests/ServicesTests/AccessHistoryServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using Application.Dtos.Responses;
+using Application.Interfaces;
+using Application.Interfaces.Repositories;
+using Application.Services;
+using FluentAssertions;
+using Infrastructure.Persistence.Tests.DbSetMocks;
+using Moq;
+
+namespace Application.Tests.ServicesTests
+{
+    public class AccessHistoryServiceTests
+    {
+        private readonly IAccessHistoryService _sut;
+        private readonly Mock<IAccessHistoryRepository> _accessHistoryRepository;
+
+        public AccessHistoryServiceTests()
+        {
+            _accessHistoryRepository = new Mock<IAccessHistoryRepository>();
+            _sut = new AccessHistoryService(_accessHistoryRepository.Object);
+        }
+
+        [Fact]
+        public async Task Test_AddAccessHistoryAsync()
+        {
+            // arrange
+            _accessHistoryRepository.Setup(x => x.AddAccessHistoryAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).ReturnsAsync(true);
+
+            // act
+            var result = await _sut.AddAccessHistoryAsync(1, 1, true);
+
+            // assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_GetAccessHistoryAsync_When_DoorId_Is_Valid()
+        {
+            // arrange
+            var timestamp = DateTime.UtcNow;
+            int doorId = 2;
+            var userAccessHistoryMocks = AccessHistoryMock.GetAccessHistoryMockData(timestamp);
+
+            _accessHistoryRepository.Setup(x => x.GetAccessHistoryAsync(It.IsAny<int>())).ReturnsAsync(userAccessHistoryMocks.Object.Where(x => x.DoorId == doorId));
+
+            // act
+            var result = await _sut.GetAccessHistoryAsync(2);
+
+            // assert
+            var expectedResult = new List<AccessHistoryResponse>()
+            {
+                new AccessHistoryResponse{UserId=1, DoorId=2,AccessGranted=true,Timestamp=timestamp, Doorname="Storage", Username="Rameez" },
+                new AccessHistoryResponse{UserId=2, DoorId=2,AccessGranted=true,Timestamp=timestamp, Doorname="Storage", Username="Darjan" }
+            };
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public async Task Test_GetAccessHistoryAsync_When_DoorId_Is_Invalid()
+        {
+            // arrange
+            var timestamp = DateTime.UtcNow;
+            int doorId = 5;
+            var userAccessHistoryMocks = AccessHistoryMock.GetAccessHistoryMockData(timestamp);
+
+            _accessHistoryRepository.Setup(x => x.GetAccessHistoryAsync(It.IsAny<int>())).ReturnsAsync(userAccessHistoryMocks.Object.Where(x => x.DoorId == doorId));
+
+            // act
+            var result = await _sut.GetAccessHistoryAsync(5);
+
+            // assert
+
+            result.Should().BeNullOrEmpty();
+        }
+    }
+}
