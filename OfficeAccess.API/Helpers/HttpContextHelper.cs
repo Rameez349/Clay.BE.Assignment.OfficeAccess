4278a5dd6eaf4739f4747798ee7e60d7159b9920
Rameez Ahmed <rameezahmed349@hotmail.com>
2023-01-09 15:45:31 +0500

    Fixed integrations tests based on refactoring.

--- OfficeAccess.API/Helpers/HttpContextHelper.cs
+++ OfficeAccess.API/Helpers/HttpContextHelper.cs
@@ -1,30 +1,29 @@
 ﻿using Application.Dtos.Responses;
 using Domain.Constants;
 
-namespace OfficeAccess.API.Helpers
+namespace OfficeAccess.API.Helpers;
+
+public static class HttpContextHelper
 {
-    public static class HttpContextHelper
+    private static HttpContext HttpContext => new HttpContextAccessor().HttpContext!;
+
+    public static long GetUserIdFromClaims()
     {
-        private static HttpContext HttpContext => new HttpContextAccessor().HttpContext!;
+        var userClaim = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "UserId");
 
-        public static int GetUserIdFromClaims()
-        {
-            var userClaim = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "UserId");
+        if (userClaim is null)
+            throw new UnauthorizedAccessException($"{ApiResponseMessages.Unauthorized}");
 
-            if (userClaim is null)
-                throw new UnauthorizedAccessException($"{ApiResponseMessages.Unauthorized}");
+        return Convert.ToInt64(userClaim.Value);
+    }
 
-            return Convert.ToInt32(userClaim.Value);
-        }
+    public static AccessResponse GetAccessResponseFromAuthorizationContext()
+    {
+        var accessResponse = (AccessResponse?)HttpContext.Items["AccessResponse"];
 
-        public static AccessResponse GetAccessResponseFromAuthorizationContext()
-        {
-            var accessResponse = (AccessResponse?)HttpContext.Items["AccessResponse"];
+        if (accessResponse is null)
+            throw new NullReferenceException(nameof(accessResponse));
 
-            if (accessResponse is null)
-                throw new NullReferenceException(nameof(accessResponse));
-
-            return accessResponse;
-        }
+        return accessResponse;
     }
 }
